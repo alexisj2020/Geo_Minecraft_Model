20e58dff145822b4045393961a0019390676f069
Alexis Johnson <alexisj@mun.ca>
2026-03-28 13:14:32 -0230

    Code that reads Minecraft blocks into numpy array

--- Sbsrf_collection.py
+++ Sbsrf_collection.py
@@ -2,26 +2,30 @@ import numpy as np
 from numpy import random
 import pandas as pd
 import sys
-
-from gdpc import __url__, Editor, Block
+from gdpc import __url__, Editor, Block, Rect
 
 editor = Editor(buffering=True)
 
-sub_grid_init = np.loadtxt("sub_grid.csv",delimiter=",", dtype=object)
-sub_grid = sub_grid_init.reshape((256, 256, 64))
+sub_grid_init = np.loadtxt("grav_test_sf.csv",delimiter=" ", dtype=object)
+sub_grid = sub_grid_init.reshape((200, 200, 44))
+#sub_grid = np.zeros(shape=(200,200,44),dtype=object)
 
 print("What z value would you like to record?:")
-zs = input()
+zs = sys.argv[1]
 z=int(zs)
+print("Please record line "+str(z))
 
-for x in range(0,256):
-    #for z in range(0, 256):
-    for y in range(-63, 1):
-        sub_grid[z, x, 64+(y-1)] = str(editor.getBlock((x, y, z)))
+rect = Rect((z,0), ((z+1),201))
+
+worldSlice = editor.loadWorldSlice(rect)
+
+for x in range(0,200):
+    for y in range(20, 64):
+        sub_grid[z, x, 63-y] = str(editor.getBlock((x, y, z)))
 
 #saving array as csv file to pull in other code
 sub_reshaped = sub_grid.reshape(sub_grid.shape[0], -1)
 
-np.savetxt("sub_grid.csv", sub_reshaped, delimiter=",", fmt="%s")
+np.savetxt("grav_test_sf.csv", sub_reshaped, delimiter=" ", fmt="%s")
 
-print("Great! Row "+zs+" has been added to sub_grid.csv.")
+print("Great! Row "+zs+" has been added to grav_test_sf.csv.")
