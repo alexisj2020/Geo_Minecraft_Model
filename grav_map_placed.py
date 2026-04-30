b0189a7cdfb445b7b1e766d6637abc4ea6b1894f
Alexis Johnson <alexisj@mun.ca>
2026-04-30 16:32:26 -0230

    updating geophys map files

--- grav_map_placed.py
+++ grav_map_placed.py
@@ -4,56 +4,63 @@ import sys
 from gdpc import __url__, Editor, Block
 editor = Editor(buffering=True)
 
+#load in gravity data
 grav_grid = np.loadtxt('grav_grid.txt', delimiter=' ',dtype=float)
 
+#place glass blocks in sky of minecraft world above play area
+#colours correspond to gravity data values
+#the distribution of colours will depend on the data range
 for x in range(0,200):
     for y in range(0,200):
-        for z in range(150,151):
-            if 0<grav_grid[y,x]<25:
+        for z in range(100,101):
+            if 0<grav_grid[y,x]<=50:
                 editor.placeBlock((x,z,y), Block("red_stained_glass"))
-            if 25<grav_grid[y,x]<50:
+            if 50<grav_grid[y,x]<=100:
                 editor.placeBlock((x,z,y), Block("orange_stained_glass"))
-            if 50<grav_grid[y,x]<75:
+            if 100<grav_grid[y,x]<=150:
                 editor.placeBlock((x,z,y), Block("yellow_stained_glass"))
-            if 75<grav_grid[y,x]<110:
+            if 150<grav_grid[y,x]<=200:
                 editor.placeBlock((x,z,y), Block("lime_stained_glass"))
-            if 110<grav_grid[y,x]<150:
+            if 200<grav_grid[y,x]<=250:
                 editor.placeBlock((x,z,y), Block("green_stained_glass"))
-            if 150<grav_grid[y,x]<200:    
+            if 250<grav_grid[y,x]<=300:    
                 editor.placeBlock((x,z,y), Block("cyan_stained_glass"))
-            if 200<grav_grid[y,x]<260:
+            if 300<grav_grid[y,x]<=400:
                 editor.placeBlock((x,z,y), Block("light_blue_stained_glass"))
-            if 260<grav_grid[y,x]<330:
+            if 400<grav_grid[y,x]<=500:
                 editor.placeBlock((x,z,y), Block("blue_stained_glass"))
-            if 330<grav_grid[y,x]<390:
+            if 500<grav_grid[y,x]<=600:
                 editor.placeBlock((x,z,y), Block("purple_stained_glass"))
-            if 390<grav_grid[y,x]<460:
+            if 600<grav_grid[y,x]<=700:
                 editor.placeBlock((x,z,y), Block("magenta_stained_glass"))
-            if 460<grav_grid[y,x]:
+            if 700<grav_grid[y,x]:
                 editor.placeBlock((x,z,y), Block("pink_stained_glass"))
 
+
+#placing carpet above the glass blocks using the same colour scheme
+#using both glass and carpet results in best appearance
 for x in range(0,200):
     for y in range(0,200):
-        for z in range(151,152):
-            if 0<grav_grid[y,x]<25:
+        for z in range(101,102):
+            if 0<grav_grid[y,x]<=50:
                 editor.placeBlock((x,z,y), Block("red_carpet"))
-            if 25<grav_grid[y,x]<50:
+            if 50<grav_grid[y,x]<=100:
                 editor.placeBlock((x,z,y), Block("orange_carpet"))
-            if 50<grav_grid[y,x]<75:
+            if 100<grav_grid[y,x]<=150:
                 editor.placeBlock((x,z,y), Block("yellow_carpet"))
-            if 75<grav_grid[y,x]<110:
+            if 150<grav_grid[y,x]<=200:
                 editor.placeBlock((x,z,y), Block("lime_carpet"))
-            if 110<grav_grid[y,x]<150:
+            if 200<grav_grid[y,x]<=250:
                 editor.placeBlock((x,z,y), Block("green_carpet"))
-            if 150<grav_grid[y,x]<200:    
+            if 250<grav_grid[y,x]<=300:    
                 editor.placeBlock((x,z,y), Block("cyan_carpet"))
-            if 200<grav_grid[y,x]<260:
+            if 300<grav_grid[y,x]<=400:
                 editor.placeBlock((x,z,y), Block("light_blue_carpet"))
-            if 260<grav_grid[y,x]<330:
+            if 400<grav_grid[y,x]<=500:
                 editor.placeBlock((x,z,y), Block("blue_carpet"))
-            if 330<grav_grid[y,x]<390:
+            if 500<grav_grid[y,x]<=600:
                 editor.placeBlock((x,z,y), Block("purple_carpet"))
-            if 390<grav_grid[y,x]<460:
+            if 600<grav_grid[y,x]<=700:
                 editor.placeBlock((x,z,y), Block("magenta_carpet"))
-            if 460<grav_grid[y,x]:
+            if 700<grav_grid[y,x]:
                 editor.placeBlock((x,z,y), Block("pink_carpet"))
--- mag_map_placed.py
+++ mag_map_placed.py
@@ -4,56 +4,62 @@ import sys
 from gdpc import __url__, Editor, Block
 editor = Editor(buffering=True)
 
-mag_grid = np.loadtxt('mag_test_1.txt', delimiter=' ',dtype=float)
+#load in magnetic data
+mag_grid = np.loadtxt('mag_data_2.txt', delimiter=' ',dtype=float)
 
+#place glass blocks in sky of minecraft world above play area
+#colours correspond to gravity data values
+#the distribution of colours will depend on the data range
 for x in range(0,200):
     for y in range(0,200):
-        for z in range(150,151):
-            if -1<mag_grid[y,x]<0:
+        for z in range(100,101):
+            if -1<mag_grid[x,y]<=2:
                 editor.placeBlock((x,z,y), Block("red_stained_glass"))
-            if 0<mag_grid[y,x]<3:
+            if 2<mag_grid[x,y]<=4:
                 editor.placeBlock((x,z,y), Block("orange_stained_glass"))
-            if 3<mag_grid[y,x]<6:
+            if 4<mag_grid[x,y]<=6:
                 editor.placeBlock((x,z,y), Block("yellow_stained_glass"))
-            if 6<mag_grid[y,x]<9:
+            if 6<mag_grid[x,y]<=8:
                 editor.placeBlock((x,z,y), Block("lime_stained_glass"))
-            if 9<mag_grid[y,x]<12:
+            if 8<mag_grid[x,y]<=10:
                 editor.placeBlock((x,z,y), Block("green_stained_glass"))
-            if 12<mag_grid[y,x]<15:    
+            if 10<mag_grid[x,y]<=12:    
                 editor.placeBlock((x,z,y), Block("cyan_stained_glass"))
-            if 15<mag_grid[y,x]<18:
+            if 12<mag_grid[x,y]<=14:
                 editor.placeBlock((x,z,y), Block("light_blue_stained_glass"))
-            if 18<mag_grid[y,x]<21:
+            if 14<mag_grid[x,y]<=16:
                 editor.placeBlock((x,z,y), Block("blue_stained_glass"))
-            if 21<mag_grid[y,x]<24:
+            if 16<mag_grid[x,y]<=18:
                 editor.placeBlock((x,z,y), Block("purple_stained_glass"))
-            if 24<mag_grid[y,x]<27:
+            if 18<mag_grid[x,y]<=20:
                 editor.placeBlock((x,z,y), Block("magenta_stained_glass"))
-            if 27<mag_grid[y,x]:
+            if 20<mag_grid[x,y]:
                 editor.placeBlock((x,z,y), Block("pink_stained_glass"))
 
+#placing carpet above the glass blocks using the same colour scheme
+#using both glass and carpet results in best appearance
 for x in range(0,200):
     for y in range(0,200):
-        for z in range(151,152):
-            if -1<mag_grid[y,x]<0:
+        for z in range(101,102):
+            if -1<mag_grid[x,y]<=2:
                 editor.placeBlock((x,z,y), Block("red_carpet"))
-            if 0<mag_grid[y,x]<3:
+            if 2<mag_grid[x,y]<=4:
                 editor.placeBlock((x,z,y), Block("orange_carpet"))
-            if 3<mag_grid[y,x]<6:
+            if 4<mag_grid[x,y]<=6:
                 editor.placeBlock((x,z,y), Block("yellow_carpet"))
-            if 6<mag_grid[y,x]<9:
+            if 6<mag_grid[x,y]<=8:
                 editor.placeBlock((x,z,y), Block("lime_carpet"))
-            if 9<mag_grid[y,x]<12:
+            if 8<mag_grid[x,y]<=10:
                 editor.placeBlock((x,z,y), Block("green_carpet"))
-            if 12<mag_grid[y,x]<15:    
+            if 10<mag_grid[x,y]<=12:    
                 editor.placeBlock((x,z,y), Block("cyan_carpet"))
-            if 15<mag_grid[y,x]<18:
+            if 12<mag_grid[x,y]<=14:
                 editor.placeBlock((x,z,y), Block("light_blue_carpet"))
-            if 18<mag_grid[y,x]<21:
+            if 14<mag_grid[x,y]<=16:
                 editor.placeBlock((x,z,y), Block("blue_carpet"))
-            if 21<mag_grid[y,x]<24:
+            if 16<mag_grid[x,y]<=18:
                 editor.placeBlock((x,z,y), Block("purple_carpet"))
-            if 24<mag_grid[y,x]<27:
+            if 18<mag_grid[x,y]<=20:
                 editor.placeBlock((x,z,y), Block("magenta_carpet"))
-            if 27<mag_grid[y,x]:
+            if 20<mag_grid[x,y]:
                 editor.placeBlock((x,z,y), Block("pink_carpet"))
